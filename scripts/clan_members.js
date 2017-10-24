7c2be660ba61e33c69b2498551c1e29bcb5e1892
Unknown <wong4a@gmail.com>
2017-10-24 15:15:29 -0500

    Removed Unused Files and Commands

--- scripts/clan_members.js
+++ scripts/clan_members.js
@@ -1,21 +1,2 @@
-var $matches = document.getElementById('matches');
-
-Starfire('client.clashRoyale.match.getAllByTag', {tag: 'PCV8'}, function (err, matches) {
-  matches.forEach(function(match) {
-    console.log(match);
-    $match = document.createElement('div');
-    $match.className = 'match';
-
-    $type = document.createElement('div');
-    $type.innerText = match.type;
-
-    $vs = document.createElement('div');
-    $vs.innerText = match.team[0].name + ' (' + match.team[0].crowns + ')';
-    $vs.innerText += ' vs ';
-    $vs.innerText += match.opponent[0].name + ' (' + match.opponent[0].crowns + ')';
-
-    $match.appendChild($type);
-    $match.appendChild($vs);
-    $matches.appendChild($match);
-  });
-});
+Starfire 'client.clashRoyale.clanRecord.getAllByTag', {limit: 10, tag: 'YU2RQG9'}, (err, clanRecords) ->
+  console.log 'clanRecords', clanRecords, err
